9b5b4b122d88e4ac220ca1e245357b53bb5c5292
3zequiel3 <3zegonzalez@gmail.com>
2025-11-03 02:06:04 +0000

    arreglo de importacion

--- src/model/Review.js
+++ src/model/Review.js
@@ -1,13 +1,13 @@
-import mongooose from 'mongoose';
+import mongoose from 'mongoose';
 
-const reviewSchema = new mongooose.Schema({
-    usuario: { type: mongooose.Schema.Types.ObjectId, ref: 'User', required: true },
-    producto: { type: mongooose.Schema.Types.ObjectId, ref: 'Product', required: true },
+const reviewSchema = new mongoose.Schema({
+    usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    producto: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
     calificacion: { type: Number, required: true, min: 1, max: 5 },
     comentario: { type: String }
 }, { timestamps: true });
 
 
-const Review = mongooose.model('Review', reviewSchema);
+const Review = mongoose.model('Review', reviewSchema);
 
 export default Review;
