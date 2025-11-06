aed92295ca1e793c093c0a4c3beb3351b466e6a8
3zequiel3 <3zegonzalez@gmail.com>
2025-11-06 22:50:21 +0000

    Routes comentadas

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,13 +1,26 @@
 import express from "express"
 import userController from "../controllers/userController.js"
-import { authenticate, checkAlredyLoggedIn } from "../middleware/auth.js"
+import { adminAuthenticate, authenticate, checkAlredyLoggedIn } from "../middleware/auth.js"
 
 const router = express.Router()
 
+//Rutas de usuarios
+//Crear usuario
+//Rutas protegidas que requieren autenticación mediante el middlewarE 'authenticate'
 router.post("/", userController.create)
+//Login y Logout
+/**
+ * La ruta de login utiliza el middleware 'checkAlredyLoggedIn' para evitar inicios de sesión redundantes.
+ */
 router.post("/login",checkAlredyLoggedIn,userController.login)
+/**
+ * La ruta de logout utiliza el middleware 'authenticate' para asegurar que solo usuarios autenticados puedan cerrar sesión.
+ */
 router.post("/logout",authenticate,userController.logout)
-router.get("/",authenticate,userController.getAll)
+/**
+ * Las rutas para obtener todos los usuarios, obtener un usuario por ID, actualizar y eliminar usuarios están protegidas por el middleware 'authenticate'.
+ */
+router.get("/",authenticate,adminAuthenticate,userController.getAll)
 router.get("/:id",authenticate, userController.getById)
 router.put("/:id",authenticate, userController.update)
 router.delete("/:id",authenticate, userController.delete)
